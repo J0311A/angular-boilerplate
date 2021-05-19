61e918bd931f89cb4b6960a2262f267aa9d27b5c
Pinto, Manuel <manuel_pinto@dellteam.com>
2021-05-19 17:24:24 +0530

    Install AG Grid

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ClarityModule } from '@clr/angular';
+import { AgGridModule } from 'ag-grid-angular';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, ClarityModule, BrowserAnimationsModule],
+  imports: [BrowserModule, AppRoutingModule, ClarityModule, BrowserAnimationsModule, AgGridModule.withComponents([])],
   providers: [],
   bootstrap: [AppComponent]
 })
